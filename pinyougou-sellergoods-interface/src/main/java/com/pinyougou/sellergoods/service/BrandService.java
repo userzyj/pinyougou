52478d08394fe3a62e77cd24dcef049474a77b3b
zyj <zyj@itheima>
2019-02-13 21:58:14 +0800

    初始化 部署

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,16 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**品牌服务处接口
+ *
+ */
+public interface BrandService  {
+    /**
+     * 返回全部列表
+     *
+     */
+    public List<TbBrand>  findAll();
+}
